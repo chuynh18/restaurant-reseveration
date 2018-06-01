5b2db972b0cae07a0cd5ccd1a0ee9f6f7f85ed55
Acer <38088640+chuynh18@users.noreply.github.com>
2018-06-01 12:06:43 -0700

    mostly finished version

--- server.js
+++ server.js
@@ -62,12 +62,8 @@ app.post("/api/reserve", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body-parser middleware
     var newReservation = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newReservation
-    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
-  
     console.log(newReservation);
+  
     if (responseObject.reservations.length < 4) {
         responseObject.reservations.push(newReservation);
     res.json(newReservation);
